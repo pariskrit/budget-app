bf8746301c0e7f46eadbe898df6686099c8e7743
pariskrit <moktanpariskrit@gmail.com>
2021-12-25 14:12:03 +0545

    starting bar chart

--- src/modules/BarPieChart/index.js
+++ src/modules/BarPieChart/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.scss";
 
 function BarPieChart() {
-  return <div className="barchart">Bar chart</div>;
+  return <div className="barchart">Lets start Bar chart</div>;
 }
 
 export default BarPieChart;
