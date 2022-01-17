1904ac2ef86ed2d87944792e7c253f406d78fd2b
pariskrit <moktanpariskrit@gmail.com>
2022-01-17 13:09:45 +0545

    fixed minussign bug

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,14 +2,17 @@ import { months } from "../constants";
 
 export const addCommaInNumbers = (number) => {
   let numberInStringArray = String(number).split("");
+  let minusSign = [];
 
   if (numberInStringArray[0] === "-") {
-    numberInStringArray.splice(0, 1);
+    minusSign = numberInStringArray.splice(0, 1);
   }
 
   switch (numberInStringArray.length) {
     case 4:
-      const slicedArray = numberInStringArray.splice(0, 1);
+      const slicedArray = minusSign.length
+        ? [minusSign[0], ...numberInStringArray.splice(0, 1)]
+        : numberInStringArray.splice(0, 1);
       const numberWithComma = [
         ...slicedArray,
         ",",
@@ -17,13 +20,14 @@ export const addCommaInNumbers = (number) => {
       ].join("");
       return numberWithComma;
     case 5:
-      const slicedArrayWithTwoDigit = numberInStringArray.splice(0, 2);
+      const slicedArrayWithTwoDigit = minusSign.length
+        ? [minusSign[0], ...numberInStringArray.splice(0, 2)]
+        : numberInStringArray.splice(0, 2);
       const numberWithComma1 = [
         ...slicedArrayWithTwoDigit,
         ",",
         ...numberInStringArray,
       ].join("");
-      // console.log(slicedArrayWithTwoDigit, numberInStringArray);
       return numberWithComma1;
 
     default:
